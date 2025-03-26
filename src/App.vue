ac2306e8e6f60d8195a953e9c95dc5bb0a67ba91
Anton Sizov <asizov.ps@gmail.com>
2025-03-26 09:33:43 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  Vue Mfe #1
+</template>
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import federation from '@originjs/vite-plugin-federation';
+
+export default defineConfig({
+  build: {
+    minify: false,
+    cssCodeSplit: false,
+    target: ['chrome89', 'edge89', 'firefox89', 'safari15'],
+  },
+  server: {
+    port: 5005,
+  },
+  preview: {
+    port: 5005,
+  },
+  plugins: [
+    vue(),
+    federation({
+      name: 'vue-mfe1',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.vue',
+      },
+      shared: ['vue'],
+    }),
+  ],
+  alias: {
+    find: '@',
+    replacement: path.resolve(__dirname, './src'),
+  },
+})
